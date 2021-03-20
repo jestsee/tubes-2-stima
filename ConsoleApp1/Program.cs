4cd0575abd51a9dff0eba58559a8e622c4a621fe
jestsee <jestsee@gmail.com>
2021-03-20 10:23:33 +0700

    tes msagl

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
-using System.Collections.Generic;
-
+using System.Windows.Forms;
 namespace tubes2stima
 {
     class Driver
@@ -9,6 +8,7 @@ namespace tubes2stima
         // Main
         public static void Main(String[] args)
         {
+            /*
             FileHandler f = new FileHandler();
             DFS dfs = new DFS();
             friendRecommendation fren = new friendRecommendation();
@@ -44,7 +44,37 @@ namespace tubes2stima
             // tes bfs
             Console.WriteLine("\ntes bfs");
             test.printBFSPath(pengguna1, pengguna2, g);
-            Console.ReadKey();
+            Console.ReadKey();*/
+
+
+            //msagl
+            //create a form 
+            System.Windows.Forms.Form form = new System.Windows.Forms.Form();
+            //create a viewer object 
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            //create a graph object 
+            Microsoft.Msagl.Drawing.Graph graph = new Microsoft.Msagl.Drawing.Graph("graph");
+            //create the graph content 
+            graph.AddEdge("A", "B");
+            graph.AddEdge("B", "C");
+            graph.AddEdge("A", "C").Attr.Color = Microsoft.Msagl.Drawing.Color.Green;
+            graph.FindNode("A").Attr.FillColor = Microsoft.Msagl.Drawing.Color.Magenta;
+            graph.FindNode("B").Attr.FillColor = Microsoft.Msagl.Drawing.Color.MistyRose;
+            Microsoft.Msagl.Drawing.Node c = graph.FindNode("C");
+            c.Attr.FillColor = Microsoft.Msagl.Drawing.Color.PaleGreen;
+            c.Attr.Shape = Microsoft.Msagl.Drawing.Shape.Diamond;
+            //bind the graph to the viewer 
+            viewer.Graph = graph;
+            //associate the viewer with the form 
+            form.SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            form.Controls.Add(viewer);
+            form.ResumeLayout();
+            //show the form 
+            form.ShowDialog();
+
+
+
         }
     }
 }
